df510e66a90e3bd7e2eb8cf7fbc6149929be92bd
DrGrim <lucifron_n@yahoo.com>
2017-04-07 01:05:34 +0300

    Add files via upload

---
+++ addLimiter.js
@@ -0,0 +1,32 @@
+function get_hostname(url) {
+ 
+  
+ var m = url.match(/^http:\/\/[^/]+/);
+   
+ return m ? m[0] : null;
+
+
+}
+
+
+
+$('iframe').each(function(){
+  
+   $(this).attr('sandbox','allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation');	
+ 
+});
+
+
+setInterval(function(){  
+
+var comp = new RegExp(get_hostname(window.location.href));
+
+$('a').each(function(){
+   if(!comp.test($(this).attr('href'))){ 
+       $(this).remove();
+   }
+});
+
+
+
+}, 50);
